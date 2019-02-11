0c20dabd6ad9b44e2aa431ac1516bfc347d7773b
Francososa <franky.sosa@gmail.com>
2019-02-10 20:19:02 -0600

    Add posterior distribution calculation and plot

--- coin_flipping_problem.py
+++ coin_flipping_problem.py
@@ -53,5 +53,37 @@ for i in range(4):
 
 ax[1,0].set_yticks([])
 ax[1,0].set_xticks([0, 0.5, 1])
-f.text(0.5, 0.5, 'Theta', ha='center')
-f.text(0.07, 0.5, 'p(Theta)', va='center', rotation=0)
+f.text(0.5, 0.05, 'theta', ha='center')
+f.text(0.07, 0.5, 'p(theta)', va='center', rotation=0)
+
+
+
+plt.figure(figsize=(10, 8))
+
+n_trials = [0, 1, 2, 3, 4, 8, 16, 32, 50, 150]
+data = [0, 1, 1, 1, 1, 4, 6, 9, 13, 48]
+theta_real = 0.35
+
+beta_params = [(1, 1), (20, 20), (1, 4)]
+dist = stats.beta
+x = np.linspace(0, 1, 200)
+
+for idx, N in enumerate(n_trials):
+    if idx == 0:
+        plt.subplot(4, 3, 2)
+        plt.xlabel('θ')
+    else:
+        plt.subplot(4, 3, idx+3)
+        plt.xticks([])
+    y = data[idx]
+    for (a_prior, b_prior) in beta_params:
+        p_theta_given_y = dist.pdf(x, a_prior + y, b_prior + N - y)
+        plt.fill_between(x, 0, p_theta_given_y, alpha=0.7)
+
+    plt.axvline(theta_real, ymax=0.3, color='k')
+    plt.plot(0, 0, label=f'{N:4d} trials\n{y:4d} heads', alpha=0)
+    plt.xlim(0, 1)
+    plt.ylim(0, 12)
+    plt.legend()
+    plt.yticks([])
+plt.tight_layout()
