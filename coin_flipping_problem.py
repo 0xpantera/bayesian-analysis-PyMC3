b9b2f1b3d1f4e17ecf152106ace49a462bb70d4d
Francososa <franky.sosa@gmail.com>
2019-02-10 19:55:52 -0600

    Add prior beta distribution

--- coin_flipping_problem.py
+++ coin_flipping_problem.py
@@ -33,4 +33,25 @@ for i in range (len(n_params)):
         ax[0,0].set_xticks(x)
 
 
+params = [0.5, 1, 2, 3]
+x = np.linspace(0, 1, 100)
+f, ax = plt.subplots(len(params), len(params), sharex=True,
+                     sharey=True,
+                     figsize=(8, 7), constrained_layout=True)
+
+
+for i in range(4):
+    for j in range(4):
+        a = params[i]
+        b = params[j]
         
+        y = stats.beta(a, b).pdf(x)
+        
+        ax[i,j].plot(x, y)
+        ax[i,j].plot(0, 0, label=f"alpha = {a}\nbeta = {b}", alpha=0)
+        ax[i,j].legend()
+
+ax[1,0].set_yticks([])
+ax[1,0].set_xticks([0, 0.5, 1])
+f.text(0.5, 0.5, 'Theta', ha='center')
+f.text(0.07, 0.5, 'p(Theta)', va='center', rotation=0)
